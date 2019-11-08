0e5f43a8ca7f78fa84612ea1ea2fde7fac67ff2c
Andrew Jolivette <amjolivette@gmail.com>
2019-11-07 17:18:39 -0800

    Updates

---
+++ precache-manifest.2e19490f6e2c37c6e4493ee72caf5eae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55f344ba730df5288165a539075718f3",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "f1ee6320c30d4a344d58",
+    "url": "/clicky-game/static/css/main.6663a23c.chunk.css"
+  },
+  {
+    "revision": "66f37b116261d3008300",
+    "url": "/clicky-game/static/js/2.78a8d61d.chunk.js"
+  },
+  {
+    "revision": "f1ee6320c30d4a344d58",
+    "url": "/clicky-game/static/js/main.1d3c85a2.chunk.js"
+  },
+  {
+    "revision": "66f7d78a04a2756f551e",
+    "url": "/clicky-game/static/js/runtime-main.ad92ec2e.js"
+  }
+]);
